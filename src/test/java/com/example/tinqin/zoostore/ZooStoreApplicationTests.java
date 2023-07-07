90b9dbb4ec5dba5dd5b31e4b61246bc6db511bbe
SimeonStoyanov01 <simosstoyanov@gmail.com>
2023-07-07 12:54:30 +0300

    Initial Commit #2

---
+++ src/test/java/com/example/tinqin/zoostore/ZooStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tinqin.zoostore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
